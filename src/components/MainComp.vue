00d842a115f832aa3491cb52743f23953f504eaa
PietroDanieli <1pie7d@gmail.com>
2022-10-02 12:28:30 +0200

    implement title, language and vote

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,18 +1,26 @@
 <template>
   <main>
     <div v-if="list.movies.length > 0">
-      <h2>My film</h2>
-      <div class="grid_col_10">
-        <div v-for="movie in list.movies" :key="movie.id">
-          <p>{{ movie.original_title }}</p>
+      <h2 class="text_center">My film</h2>
+      <br />
+      <div class="grid_col_7">
+        <div class="pad_x_2" v-for="movie in list.movies" :key="movie.id">
+          <p>Title: {{ movie.title }}</p>
+          <p>Original title:{{ movie.original_title }}</p>
+          <p>Language:{{ movie.original_language }}</p>
+          <p>Vote:{{ parseInt(movie.vote_average / 2) }}</p>
         </div>
       </div>
     </div>
     <div v-if="list.tvSeries.length > 0">
-      <h2>My series tv</h2>
-      <div class="grid_col_10">
-        <div v-for="serie in list.tvSeries" :key="serie.id">
-          <p>{{ serie.name }}</p>
+      <h2 class="text_center">My series tv</h2>
+      <br />
+      <div class="grid_col_7">
+        <div class="pad_x_2" v-for="serie in list.tvSeries" :key="serie.id">
+          <p>Name {{ serie.name }}</p>
+          <p>Original name {{ serie.original_name }}</p>
+          <p>Language:{{ serie.original_language }}</p>
+          <p>Vote:{{ parseInt(serie.vote_average / 2) }}</p>
         </div>
       </div>
     </div>
@@ -28,6 +36,4 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-@import "@/assets/style/grid.scss";
-</style>
+<style lang="scss" scoped></style>

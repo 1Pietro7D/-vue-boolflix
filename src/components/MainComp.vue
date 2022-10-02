66d4ffc2d343d5dc7c8a5ff1c2c4371ece8ca67c
PietroDanieli <1pie7d@gmail.com>
2022-10-02 11:53:34 +0200

    implement in Main title for movies and tvSeries and impor grid.scss

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,10 +1,20 @@
 <template>
   <main>
     <div v-if="list.movies.length > 0">
-      <h1>My film</h1>
+      <h2>My film</h2>
+      <div class="grid_col_10">
+        <div v-for="movie in list.movies" :key="movie.id">
+          <p>{{ movie.original_title }}</p>
+        </div>
+      </div>
     </div>
     <div v-if="list.tvSeries.length > 0">
-      <h1>My series tv</h1>
+      <h2>My series tv</h2>
+      <div class="grid_col_10">
+        <div v-for="serie in list.tvSeries" :key="serie.id">
+          <p>{{ serie.name }}</p>
+        </div>
+      </div>
     </div>
   </main>
 </template>
@@ -18,4 +28,6 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "@/assets/style/grid.scss";
+</style>

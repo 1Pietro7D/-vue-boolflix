a071ada7320481cd862dabf3876ebe7a72a8853d
PietroDanieli <1pie7d@gmail.com>
2022-10-03 00:07:20 +0200

    implement for main description in cards and some css

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -4,13 +4,20 @@
       <h2 class="text_center">My film</h2>
       <br />
       <div class="grid_col_7">
-        <div class="pad_2" v-for="movie in list.movies" :key="movie.id">
-          <p>Title: {{ movie.title }}</p>
-          <p v-if="movie.title !== movie.original_title">
-            Original title:{{ movie.original_title }}
-          </p>
-          <p>Language:<FlagComp :language="movie.original_language" /></p>
-          <p>Vote:{{ parseInt(movie.vote_average / 2) }}</p>
+        <div
+          class="card_container"
+          v-for="movie in list.movies"
+          :key="movie.id"
+        >
+          <div class="card">
+            <p>Title: {{ movie.title }}</p>
+            <p v-if="movie.title !== movie.original_title">
+              Original title:{{ movie.original_title }}
+            </p>
+            <p>Language:<FlagComp :language="movie.original_language" /></p>
+            <p>Vote:{{ parseInt(movie.vote_average / 2) }}</p>
+            <p>Description: {{ movie.overview }}</p>
+          </div>
         </div>
       </div>
     </div>
@@ -18,13 +25,20 @@
       <h2 class="text_center">My series tv</h2>
       <br />
       <div class="grid_col_7">
-        <div class="pad_2" v-for="serie in list.tvSeries" :key="serie.id">
-          <p>Name {{ serie.name }}</p>
-          <p v-if="serie.title !== serie.original_title">
-            Original name {{ serie.original_name }}
-          </p>
-          <p>Language:<FlagComp :language="serie.original_language" /></p>
-          <p>Vote:{{ parseInt(serie.vote_average / 2) }}</p>
+        <div
+          class="card_container"
+          v-for="serie in list.tvSeries"
+          :key="serie.id"
+        >
+          <div class="card">
+            <p>Name {{ serie.name }}</p>
+            <p v-if="serie.title !== serie.original_title">
+              Original name {{ serie.original_name }}
+            </p>
+            <p>Language:<FlagComp :language="serie.original_language" /></p>
+            <p>Vote:{{ parseInt(serie.vote_average / 2) }}</p>
+            <p>Description: {{ serie.overview }}</p>
+          </div>
         </div>
       </div>
     </div>
@@ -42,4 +56,15 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card_container {
+  height: 400px;
+  padding: 1rem;
+  margin-bottom: 1.5rem;
+  .card {
+    overflow-y: auto;
+    background-color: aqua;
+    height: 100%;
+  }
+}
+</style>

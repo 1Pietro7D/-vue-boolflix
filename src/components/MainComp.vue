5cd055873dda266d100897e9ba6e9201f7512522
PietroDanieli <1pie7d@gmail.com>
2022-10-03 02:49:47 +0200

    implement image and with hover view overview result

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -10,6 +10,13 @@
           :key="movie.id"
         >
           <div class="card">
+            <img
+              :src="baseUrlImg + 342 + movie.poster_path"
+              alt=""
+              @error="errImg($event)"
+            />
+          </div>
+          <div class="card_back">
             <p>Title: {{ movie.title }}</p>
             <p v-if="movie.title !== movie.original_title">
               Original title:{{ movie.original_title }}
@@ -31,6 +38,13 @@
           :key="serie.id"
         >
           <div class="card">
+            <img
+              :src="baseUrlImg + 342 + serie.poster_path"
+              alt=""
+              @error="errImg($event)"
+            />
+          </div>
+          <div class="card_back">
             <p>Name {{ serie.name }}</p>
             <p v-if="serie.title !== serie.original_title">
               Original name {{ serie.original_name }}
@@ -53,6 +67,17 @@ export default {
   props: {
     list: Object,
   },
+  data() {
+    return {
+      baseUrlImg: "https://image.tmdb.org/t/p/w",
+    };
+  },
+  methods: {
+    errImg(event) {
+      event.target.src =
+        "https://www.metrorollerdoors.com.au/wp-content/uploads/2018/02/unavailable-image-300x225.jpg";
+    },
+  },
 };
 </script>
 
@@ -61,10 +86,31 @@ export default {
   height: 400px;
   padding: 1rem;
   margin-bottom: 1.5rem;
+  position: relative;
   .card {
-    overflow-y: auto;
-    background-color: aqua;
+    width: 100%;
     height: 100%;
+    img {
+      max-width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
   }
+  .card_back {
+    overflow-y: auto;
+    background-color: black;
+    height: 100%;
+    width: 100%;
+    color: white;
+    display: none;
+  }
+}
+
+.card_container:hover > .card {
+  display: none;
+}
+
+.card_container:hover > .card_back {
+  display: inline-block;
 }
 </style>
